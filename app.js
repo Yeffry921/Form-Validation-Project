1a51292c9760204ef6aa75dda71203d6aee98bc6
yeffry <yeffry45@gmail.com>
2020-05-02 12:57:15 -0400

    modified the module back to a regular functions

--- app.js
+++ app.js
@@ -1,77 +1,94 @@
+const valivateUsername = (username) => {
+    const userLength = username.toString().length;
+    return userLength <= 15 && userLength > 8 ? true : false;
+};
+
+const validateEmail = (input) => {
+    const emailReg = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+    return emailReg.test(input)
+        
+    
+};
+
+const validatePassword = (password) => {
+    const passw = new RegExp("^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*[!@#\$%\^&\*])(?=.{8,})");
+    return passw.test(password);
+
+};
+
 const form = document.querySelector('.form');
+const username = document.querySelector('#username');
+const email = document.querySelector('#email');
+const password = document.querySelector('#password');
+const password2 = document.querySelector('#password2');
 
+const showError = (input, message) => {
+    const formControl = input.parentElement;
+    formControl.className = 'form-control error';
+    const small = formControl.querySelector('small');
+    small.textContent = message;
+};
 
-const validator = (() => {
+const showSuccess = (input) => {
+    const formControl = input.parentElement;
+    formControl.className = 'form-control success';
+};
 
-    const valivateUsername = (username) => {
-        const userLength = username.toString().length;
-        return userLength <= 15 && userLength > 8 ? true : false;
+const checkRequired = (inputArr) => {
+    inputArr.forEach((input) => {
+        if (input.value === '') {
+            showError(input, `${input.id} is required`);
+        }
+    });
+};
+
+const checkUser = (user) => {
+    if(valivateUsername(user.value)){
+        showSuccess(username)
+    } else {
+        showError(username, `Username must be between 8 and 15 characters`);
     }
+};
 
-    const validateEmail = (email) => {
-        const emailReg = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
-        return emailReg.test(String(email).toLowerCase());
+const checkPassword = (password) => {
+    if(validatePassword(password.value)){
+        showSuccess(password);
+    } else {
+        showError(password, `Password must be 8 characters minimum`);
     }
+};
 
-    const validatePassword = (password) => {
-        const passw = /^[A-Za-z]\w{7,14}$/;
-        return passw.test(password);
+const checkPassword2 = (password2) => {
+    if(validatePassword(password2.value)){
+        showSuccess(password2);
+    } else {
+        showError(password2, `Password must be 8 characters minimum`);
     }
+};
 
-    const checkpasswordMatch = (password, password2) => {
-        if (password.length <= 0 || password2.length <= 0) return false
-        return password.toString() === password2.toString();
+const checkEmail = (email) => {
+    if(validateEmail(email.value)){
+        showSuccess(email);
+    } else {
+        showError(email, `Please enter a correct email`);
     }
+};
 
-    return {
-        valivateUsername,
-        validateEmail,
-        validatePassword,
-        checkpasswordMatch
-    }
-})();
-
-const usernameInput = document.querySelector('#username');
-const emailInput = document.querySelector('#email');
-const passwordInput = document.querySelector('#password');
-const password2Input = document.querySelector('#password2');
-
+const checkpasswordMatch = (password, password2) => {
+    if(password.value !== password2.value){
+        showError(password2, 'Password don\'t match')
+    } 
+};
 
 form.addEventListener('submit', (e) => {
     e.preventDefault();
-    const username = e.target.username.value;
-    const email = e.target.email.value;
-    const password = e.target.password.value;
-    const password2 = e.target.password2.value;
-
-    if (validator.valivateUsername(username)) {
-        runSuccess(usernameInput);
-    } else {
-        runFailure(usernameInput);
-    }
-
-    if (validator.validateEmail(email)) {
-        runSuccess(emailInput);
-    } else {
-        runFailure(emailInput);
-    }
-
-    if (validator.validatePassword(password)) {
-        console.log('passed')
-        if (validator.checkpasswordMatch(password, password2)) {
-            runSuccess(password2Input);
-        } else {
-            runFailure(password2Input);
-        }
-    } else {
-        
-    }
+    checkRequired([username, email, password, password2]);
+    checkUser(username);
+    checkPassword(password);
+    checkPassword2(password2);
+    checkEmail(email);
+    checkpasswordMatch(password, password2);
+    console.log(validatePassword(password.value))
+    
 })
 
-const runSuccess = (input) => {
-    input.classList.add('success');
-}
-
-const runFailure = (input) => {
-    input.classList.add('fail');
-}
