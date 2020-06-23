cdfb7b4f785385c78416a199550710384497cd30
yeffry <yeffry45@gmail.com>
2020-06-23 11:11:26 -0400

    reworked form validation logic

--- app.js
+++ app.js
@@ -1,94 +1,63 @@
-const valivateUsername = (username) => {
-    const userLength = username.toString().length;
-    return userLength <= 15 && userLength > 8 ? true : false;
+// CONDITION VALIDATIONS
+const validateEmail = (email) => {
+	const emailReg = /^[^@]+@[^@]+\.[^@]+$/;
+	return emailReg.test(email);
 };
-
-const validateEmail = (input) => {
-    const emailReg = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
-    return emailReg.test(input)
-        
-    
+const validateCountry = (string) => {
+	return string.length >= 2;
 };
-
+const validateZipCode = (number) => {
+	return number.length >= 5;
+};
 const validatePassword = (password) => {
-    const passw = new RegExp("^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*[!@#\$%\^&\*])(?=.{8,})");
-    return passw.test(password);
-
+	const passwordReg = /^(?=.*[A-Za-z])(?=.*\d)[A-Za-z\d]{8,}$/;
+	return passwordReg.test(password);
+};
+const validateMatch = (password, passwordConfirm) => {
+	if (password === passwordConfirm) {
+		return true;
+	}
+	return false;
+};
+// ALERT SUCCESS OR FAILURE
+const showSuccess = (element) => {
+  element.className = 'form-control success';
+};
+const showfailure = (element) => {
+  element.className = 'form-control fail';
+};
+const alertOutcome = (outcome, e) => {
+  const formControl = e.target.parentElement;
+	if (outcome) {
+    showSuccess(formControl);
+	} else {
+    showfailure(formControl);
+  }
+};
+// INPUT HANDLER
+const validateHandler = (e) => {
+	if (e.target.name === 'email') {
+		const testEmail = validateEmail(e.target.value);
+		alertOutcome(testEmail, e);
+	}
+	if (e.target.name === 'country') {
+		const testCountry = validateCountry(e.target.value);
+		alertOutcome(testCountry, e);
+	}
+	if (e.target.name === 'zipCode') {
+		const testZip = validateZipCode(e.target.value);
+		alertOutcome(testZip, e);
+	}
+	if (e.target.name === 'password') {
+		const testPassword = validatePassword(e.target.value);
+		alertOutcome(testPassword, e);
+	}
+	if (e.target.name === 'passwordConfirm') {
+		const password = document.querySelector('#password').value;
+		const testPassConfirm = validateMatch(password, e.target.value);
+		alertOutcome(testPassConfirm, e);
+	}
 };
 
-const form = document.querySelector('.form');
-const username = document.querySelector('#username');
-const email = document.querySelector('#email');
-const password = document.querySelector('#password');
-const password2 = document.querySelector('#password2');
-
-const showError = (input, message) => {
-    const formControl = input.parentElement;
-    formControl.className = 'form-control error';
-    const small = formControl.querySelector('small');
-    small.textContent = message;
-};
-
-const showSuccess = (input) => {
-    const formControl = input.parentElement;
-    formControl.className = 'form-control success';
-};
-
-const checkRequired = (inputArr) => {
-    inputArr.forEach((input) => {
-        if (input.value === '') {
-            showError(input, `${input.id} is required`);
-        }
-    });
-};
-
-const checkUser = (user) => {
-    if(valivateUsername(user.value)){
-        showSuccess(username)
-    } else {
-        showError(username, `Username must be between 8 and 15 characters`);
-    }
-};
-
-const checkPassword = (password) => {
-    if(validatePassword(password.value)){
-        showSuccess(password);
-    } else {
-        showError(password, `Password must be 8 characters minimum`);
-    }
-};
-
-const checkPassword2 = (password2) => {
-    if(validatePassword(password2.value)){
-        showSuccess(password2);
-    } else {
-        showError(password2, `Password must be 8 characters minimum`);
-    }
-};
-
-const checkEmail = (email) => {
-    if(validateEmail(email.value)){
-        showSuccess(email);
-    } else {
-        showError(email, `Please enter a correct email`);
-    }
-};
-
-const checkpasswordMatch = (password, password2) => {
-    if(password.value !== password2.value){
-        showError(password2, 'Password don\'t match')
-    } 
-};
-
-form.addEventListener('submit', (e) => {
-    e.preventDefault();
-    checkRequired([username, email, password, password2]);
-    checkUser(username);
-    checkPassword(password);
-    checkPassword2(password2);
-    checkEmail(email);
-    checkpasswordMatch(password, password2);
-    console.log(validatePassword(password.value))
-    
-})
-
+// EVENT LISTERNERS
+document.querySelector('.form').addEventListener('input', validateHandler);
